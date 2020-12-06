02cef70851db8a1dc6acd554123cb65bf7432eb9
wxia <qlhdtly12189@163.com>
2020-12-06 23:35:42 +0800

    组件float-button

---
+++ src/components/float-button.vue
@@ -0,0 +1,48 @@
+<template>
+  <view @click="handleClick" class="round flex justify-around align-center" :class="'bg-' + color" :style="style">
+    <text class="text-xxl text-bold" :class="'cuIcon-' + icon"></text>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    w: {
+      type: String,
+      default: '50px'
+    },
+    h: {
+      type: String,
+      default: '50px'
+    },
+    b: {
+      type: String,
+      default: '25px'
+    },
+    r: {
+      type: String,
+      default: '25px'
+    },
+    color: {
+      type: String,
+      default: 'blue'
+    },
+    icon: {
+      type: String,
+      default: 'add'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('go');
+    }
+  },
+  computed: {
+    style() {
+      return `width: ${this.w};height: ${this.h};bottom:${this.b};right:${this.r};position:fixed;box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.2);`;
+    }
+  }
+};
+</script>
+
+<style></style>
