3edf8e0b437964057be96bba7c67eb9603260078
wxia <qlhdtly12189@163.com>
2020-12-09 22:44:08 +0800

    消息和联系人首页

---
+++ src/pages/contacter/list/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>用户联系人页面</view>
+</template>
+
+<script>
+import goLoginMixins from '@/mixins/user/go-login';
+export default {
+  mixins: [goLoginMixins],
+  onShow() {
+    this.goLogin();
+  }
+};
+</script>
+
+<style></style>
---
+++ src/pages/messages/list/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>用户消息页面</view>
+</template>
+
+<script>
+import goLoginMixins from '@/mixins/user/go-login';
+export default {
+  mixins: [goLoginMixins],
+  onShow() {
+    this.goLogin();
+  }
+};
+</script>
+
+<style></style>
