241f0d971b7b844babe8b28451e0d4b054ad05b0
wxia <qlhdtly12189@163.com>
2021-02-05 23:28:57 +0800

    moment filter link

--- src/filter/moment.js
+++ src/filter/moment.js
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
+import { weekday } from '@/asserts/config';
+
 moment.locale('zh-cn');
 export default function (value, ...rest) {
   if (typeof value == 'undefined') return '';
@@ -23,7 +25,7 @@ export default function (value, ...rest) {
         case 'chat': {
           let diff = moment().diff(moment(date));
           if (diff < 1000 * 3600 * 24) {
-            value = moment(date).format('HH:mm');
+            value = moment(date).format('HH:mm:ss');
           } else {
             value = moment(date).format('MM-DD HH:mm');
           }
@@ -33,6 +35,23 @@ export default function (value, ...rest) {
           let end = moment();
           let start = moment(date);
           return end.diff(start, 'days') + '天';
+        }
+        case 'link': {
+          let currentDate = moment(date);
+          let nowDate = moment();
+          let diff = nowDate.diff(currentDate, 'minutes');
+          if (diff <= 5) {
+            value = '刚刚';
+          } else if (diff <= 60 * 24) {
+            value = currentDate.format('HH:mm');
+          } else if (diff <= 60 * 24 * 7) {
+            value = weekday[nowDate.day()];
+          } else if (currentDate.year() === nowDate.year()) {
+            value = currentDate.format('MM-DD');
+          } else {
+            value = currentDate.format('YYYY-MM-DD');
+          }
+          break;
         }
         case 'simple': {
           value = moment(date).format('MM-DD HH:mm');
